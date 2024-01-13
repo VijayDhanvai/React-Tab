9e3a2be469c1b628e8e9d878ed061a16e2cd9a8e
VijayDhanvai <dhanvai.vijay@gmail.com>
2024-01-13 17:13:45 +0530

    added Home URL

--- src/View/Layout.jsx
+++ src/View/Layout.jsx
@@ -2,25 +2,23 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 
 function Layout() {
-    return (
-      <div>
-        {/* A "layout route" is a good place to put markup you want to
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
             share across all the pages on your site, like navigation. */}
-       <Navbar/>
-  
-        <hr />
-  
-        {/* An <Outlet> renders whatever child route is currently active,
-            so you can think about this <Outlet> as a placeholder for
-            the child routes we defined above. */}
-        <div className="lg:container mx-auto px-4">
+      <Navbar />
 
-    
+      <hr />
+
+ 
+      <div className="lg:container mx-auto px-4">
+
+
         <Outlet />
 
-        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-  export default Layout;
+export default Layout;
